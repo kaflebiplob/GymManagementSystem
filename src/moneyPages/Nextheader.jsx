a75b493c6b41c5f462b86f4dd507b8d73efda9ee
Biplob kafle <biplobkafle21@gmail.com>
2024-07-14 19:59:11 +0545

    proper responsive

--- src/moneyPages/Nextheader.jsx
+++ src/moneyPages/Nextheader.jsx
@@ -1,46 +1,50 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 function LoginSignHeader() {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+const  toogleHamburger= ()=> {
+    setHamburgerOpen(!hamburgerOpen);
+  }
+  // console.log(setHamburgerOpen)
   return (
     <div className="LSheader-section">
       <div className="LSheadContainer">
         <Link to={"/gymbody"}>
           <div className="logo">Gym</div>
         </Link>
-
-        <div className="services">
+       
+        <div className={`services ${hamburgerOpen ? "show" : ""}`}>
           <ul>
             <li>
-              <Link to={"/home"}>
-                <a href="/home" id="home" className="nav-link">
-                  Home
-                </a>
+              <Link to={"/home"} className="nav-link" id="home">
+                Home
               </Link>
             </li>
 
             <li>
-              <Link to={"/aboutme"}>
-                <a href="/aboutme" id="about" className="nav-link">
-                  About
-                </a>
+              <Link to={"/aboutme"} id="about" className="nav-link">
+                about
               </Link>
             </li>
             <li>
-              <Link to={"/product"}>
-                <a href="" id="product" className="nav-link">
-                  {" "}
-                  Product
-                </a>
+              <Link to={"/product"} id="product" className="nav-link">
+                Product
+                
               </Link>
             </li>
             <li>
-              <Link to={"/contact"}>
-                <a href="" id="contact" className="nav-link">
-                  Contact
-                </a>
+              <Link to={"/contact"} className="nav-link" id="contact">
+                Contact
               </Link>
             </li>
           </ul>
+        </div>
+        <div className="hamburger" onClick={toogleHamburger}>
+          <div className="hamburger-arrow"></div>
+          <div className="hamburger-arrow"></div>
+          <div className="hamburger-arrow"></div>
         </div>
       </div>
     </div>
